1287817ca4452406ae568c166bbbf7f2f20ef72e
Vitalii Pertsovych <oddisey000@gmail.com>
2022-10-18 18:08:14 +0300

    Initial commit

---
+++ src/components/pages/main/main.page.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import AppBarComponent from "../../shared/app-bar/app-bar.component";
+
+const MainPageComponent = () => {
+  return (
+    <React.Fragment>
+      <AppBarComponent />
+    </React.Fragment>
+  )
+};
+
+export default MainPageComponent;
---
+++ src/components/shared/app-bar/app-bar.component.jsx
@@ -0,0 +1,85 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import MenuItem from '@mui/material/MenuItem';
+import logo from '../../../img/logo.png';
+
+const pages = ['Послуги', 'Новини', 'Контакти'];
+
+const AppBarComponent = () => {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  return (
+    <AppBar position="static" style={{ backgroundColor: '#fff', background: 'cornsilk', boxShadow: "none" }}>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters style={{ flexDirection: 'row-reverse' }}>
+          <Box style={{ flexDirection: 'inherit' }} sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              style={{ backgroundColor: '#ff3d00' }}
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <img src={logo} alt="company-logo" style={{width: "10rem", padding: "1rem"}} />
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: '#000', display: 'block' }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
+export default AppBarComponent;
